f2b4fe4513e34bc4fa7c13835a5befab1268e9d2
A Mon <a.mont.24.777@gmail.com>
2024-01-26 18:44:05 -0600

    Update

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -1,20 +1,29 @@
-
 #![no_std]
 
 use gstd::{ prelude::*, ActorId };
 use gmeta::{In,Out,InOut,Metadata};
 
 
-
 // 1. Create your own Actions
-#[derive(Encode, Decode, TypeInfo, Hash, PartialEq, PartialOrd, Eq, Ord, Clone, Copy, Debug)]
+#[derive(Encode, Decode, TypeInfo,  Clone)]
 pub enum Action {
     
     // Add Actions
-    FirstAction,
-    SecondAction,
-    ThirdAction,
+    FirstAction, // Example an action with a simple input
+    SecondAction(String), // Example an action with a u128 input
+    ThirdAction(u128), // Example an action with a String input
+    Fourthaction(CustomInput), // Example an action with a custom input
+  
     
+}
+
+// Example of a custom input for an action
+#[derive(Default, Encode, Decode, Clone, TypeInfo)]
+pub struct CustomInput {
+    firstfield: String,
+    secondfield: u128,
+    thirdfield: ActorId,
+   
 }
 
 
@@ -26,15 +35,16 @@ pub enum  Event {
     FirstEvent,
     SecondEvent,
     ThirdEvent,
+    FourtEvent
 }
 
 
 // 3. Create your own Struct
-#[derive(Default, Encode, Decode, Clone, TypeInfo)]
-pub struct CustomStruct {
-    firstfield: String,
-    secondfield: u128,
-    thirdfield: ActorId,
+#[derive(Default, Clone, Encode, Decode, TypeInfo)]
+pub struct IoCustomStruct {
+   pub firstfield: String,
+   pub secondfield: u128,
+   pub thirdfield: Vec<(ActorId,u128)>
    
 }
 
@@ -58,6 +68,6 @@ impl Metadata for ContractMetadata{
      type Others = ();
      type Reply=();
      type Signal = ();
-     type State = Out<CustomStruct>;
+     type State = Out<IoCustomStruct>;
 
 }
