810bb2d7e1289daf1a628eecc358ff7fd4d763f1
A Mon <a.mont.24.777@gmail.com>
2024-04-16 17:15:34 -0600

    Update state directory

--- state/src/lib.rs
+++ state/src/lib.rs
@@ -1,10 +1,8 @@
-
 #![no_std]
 
+use gmeta::{metawasm, Metadata};
+use gstd::{prelude::*, ActorId};
 use io::*;
-use gmeta::{ Metadata, metawasm};
-use gstd::{ ActorId, prelude::*};
-
 
 #[cfg(feature = "binary-vendor")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
@@ -13,13 +11,9 @@ include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 pub mod metafns {
     pub type State = <ContractMetadata as Metadata>::State;
 
-
     // Add your State functions
 
     pub fn state(state: State) -> CustomStruct {
         state
     }
-
-
-
 }
